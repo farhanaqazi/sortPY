b1f239c4406375a9e41281ee0d9835b0d8619689
farhanaqazi <38480153+farhanaqazi@users.noreply.github.com>
2018-08-05 19:42:47 -0400

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,9 @@
+''' sorter program'''
+
+x = int(input("Enter first Number Please: "))
+y = int(input("Enter second Number Please: "))
+z = int(input("Enter third Number Please: "))
+mylist=sorted([x,y,z])
+print(mylist)
+
+
